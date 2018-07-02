b4930af4167cdfb831e4364567c72a4afcba5303
Cristina Moreno Ruiz <cmorenoruiz@educa.madrid.org>
2018-07-02 12:38:17 +0200

    Incluido un cast al leer de teclado y la condición de parada del bucle.

---
+++ TablaDeMultiplicar.py
@@ -0,0 +1,6 @@
+#Dado un número, imprime su tabla de multiplicar
+numero=int(input("Dame un número"))
+contador=1
+while contador<=10:
+    print(numero,"*",contador,"=",numero*contador)
+    contador+=1
