c7a0373b0cd262a09e24f19eb6fa9a4e9136720f
Cristina Moreno Ruiz <cmorenoruiz@educa.madrid.org>
2018-07-03 12:19:14 +0200

    Añado método para imprimir como string __str__

--- Bicicleta.py
+++ Bicicleta.py
@@ -1,9 +1,12 @@
 class Bicicleta:
-    color = ""
-    tamanio = ""
+    color = "roja"
+    tamanio = "25"
     velMax=15
     def __init__(self):
         self.velocidad = 0
+    def __str__(self):
+        return "Color: "+str(self.color)+ ", Tamaño: "+str(self.tamanio)+", Velocidad actual: "+str(self.velocidad)+" y Velocidad Máxima: "+str(self.velMax)
+
     def subirMarcha (self):
         self.velocidad = self.velocidad+1
     def bajarMarcha(self):
