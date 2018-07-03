1baee65b17c7c94881a03cf46117a434baf78b45
Cristina Moreno Ruiz <cmorenoruiz@educa.madrid.org>
2018-07-03 11:24:09 +0200

    Tratamiento de dos errores: al introducir texto y esperar un número, o al dividir por cero

--- Control de errores.py
+++ Control de errores.py
@@ -1,8 +1,14 @@
 def controlErrores():
     try:
-        numero = int(input("Introduce número:"))
-        print("Número:", numero)
+        dividendo = int(input("Introduce dividendo:"))
+        divisor = int(input("Introduce divisor:"))
+        resultado=dividendo/divisor
+        print(f"Resultado división: {resultado}")
     except ValueError:
         print("Error debes introducir un número")
         controlErrores()
+    except ZeroDivisionError:
+        print("Error no puedes dividir por cero")
+        controlErrores()
+
 controlErrores()
