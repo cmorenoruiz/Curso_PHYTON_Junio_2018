2455044a2cb7785f34f34a4c2b9481bbe714768d
Cristina Moreno Ruiz <cmorenoruiz@educa.madrid.org>
2018-07-05 12:56:07 +0200

    Al recorrer con el for el contenido del cursor, si solo queremos un valor, hay que pedírselo llamando a sa posición del array apellido[0]

--- EjercicioParaClase.py
+++ EjercicioParaClase.py
@@ -4,14 +4,14 @@ bd_conexion = bd.connect(host='localhost', port='3306',
                                    user='root', password='', database='Hospital')
 cursor = bd_conexion.cursor()
 try:
-    departamento=input("Introduce Nombre del Departamento:")
-    consulta= "SELECT emp.apellido FROM emp, dept WHERE emp.DEPT_NO=dept.DEPT_NO && dept.dnombre=%s"
-    cursor.execute(consulta,(departamento,))
+    nombre=input("Introduce nombre del Departamento:")
+    consulta= "SELECT apellido FROM emp, dept WHERE emp.DEPT_NO=dept.DEPT_NO and dnombre=%s"
+    cursor.execute(consulta,(nombre,))
+
     # Si en un único parámetro tenemos que poner ',' a continuación del valor de la variable
     resultado=False
     for apellido in cursor:
-        print("Apellido: ", apellido)
-        #print("departamento", caca)
+        print("Apellido: ", apellido[0])
         resultado=True
     if not resultado:
         print("Sin resultados para el departamento ",departamento)
